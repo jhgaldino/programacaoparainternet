f3217d223a7a556c4301d87f82c903a77746079d
Jonathan Henrique Galdino <jhenrique87@yahoo.com.br>
2021-06-23 20:05:11 -0300

    Introdução ao Javascript

--- 7 - Javascript Introdução/scripts.js
+++ 7 - Javascript Introdução/scripts.js
@@ -31,13 +31,46 @@ let result =2-4+ "Quatro";//nesse caso o mais vira concatenação
 imprime(result);
 
 imprime('estruturas condicionais')
-let idade = 19;
-if(idade >=18){
-    imprime("Voto obrigatorio");
-}else if(idade <18 || idade >65){
+// ESTRUTURA SE
+imprime("")
+imprime("-->SE")
+let idade = 12;
+if(idade < 16){
+    imprime("Voto não Permitido");
+}
+else if(idade <18 || idade >65){
     imprime("voto facultativo");
+}else{
+    imprime("Voto Obrigatório")
+}
+//ESTRUTURA SWITCH , TRABALHA EM CASCATA
+imprime("")
+imprime("-->SWITCH...CASE")
+let dia = 4;
+switch(dia){
+    case 1 : imprime("Domingo");break;
+    case 2 : imprime("Segunda");break;
+    case 3 : imprime("Terça");break;
+    case 4 : imprime("Quarta");break;
+    case 5 : imprime("Quinta");break;
+    case 6 : imprime("Sexta");break;
+    default : imprime("Sabado");break;
 }
 
+//ESTRUTURAS DE REPETIÇÃO
+//WHILE , ESTRUTURA MELHOR PARA ESTADOS (VERDADEIRO OU FALSO)
+imprime("estruturas de repetição")
+imprime("-->WHILE")
+let num = 1;
+while(num < 10){
+    imprime(num);
+    num = num+1;
+}
+//ESTRUTURA FOR , LOOP EXCLUSIVO PARA CONTADORES
+imprime("-->FOR")
+for(i=0;i<10;i++){
+    imprime(i);
+}
 
 
 
